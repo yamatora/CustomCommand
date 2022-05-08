7f7bd63b332036e0ea88be13025b5b07cb8769bc
yamatora <yt@yamadataiga.com>
2022-05-08 19:31:42 +0900

    rename

---
+++ image_splitter.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo ==Split image
+
+Python C:\Users\yamatora\CustomCommand\ImageSplitter\image_splitter.py %1 %2 %3
--- splitimage.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo ==Split image
-
-Python C:\Users\yamatora\CustomCommand\SplitImage\split_image.py %1 %2 %3
