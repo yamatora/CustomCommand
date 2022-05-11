edc1bfc884ff34bd5fc3d64f2ca7b89ed217f105
yamatora <yt@yamadataiga.com>
2022-05-11 23:28:24 +0900

    add comment

--- md2tex.bat
+++ md2tex.bat
@@ -4,4 +4,8 @@ echo ==Output .pdf from .md via .tex
 set name=%~n1
 echo "%name%.md -> %name%.pdf"
 
+@REM =番号なし
 pandoc %name%.md -o %name%.pdf --template=C:\Users\yamatora\CustomCommand\md2tex\template.tex --pdf-engine=lualatex --pdf-engine-opt=-shell-escape -V documentclass=bxjsarticle -V classoption=pandoc --listings
+
+@REM =番号あり
+@REM pandoc %name%.md -o %name%.pdf --template=C:\Users\yamatora\CustomCommand\md2tex\template.tex --pdf-engine=lualatex --pdf-engine-opt=-shell-escape -V documentclass=bxjsarticle -V classoption=pandoc --listings -N
