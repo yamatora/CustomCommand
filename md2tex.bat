0d2f643c558a588a8ee9be6b456930ce5507b593
yamatora <yt@yamadataiga.com>
2022-09-16 02:22:40 +0900

    fix

--- md2tex.bat
+++ md2tex.bat
@@ -1,12 +1,35 @@
 @echo off
 
 echo ==Output .pdf from .md via .tex
+set dir=%~dp0
 set name=%~n1
-set option=%~2
-echo "%name%.md -> %name%.pdf"
+set ARGS=
+setlocal enabledelayedexpansion
+set POS=cmd
+for %%a in (%*) do (
+  if "!pos!"=="cmd" (
+    set COMMAND=%%~a
+    set POS=arg
+  ) else if "!pos!"=="arg" (
+    set ARGS=!ARGS! %%a
+  )
+)
+echo %name%.md -> %name%.pdf
 
-@REM =番号なし
-pandoc %name%.md -o %name%.pdf --template=C:\Users\yamatora\CustomCommand\md2tex\template.tex --pdf-engine=lualatex --pdf-engine-opt=-shell-escape -V documentclass=bxjsarticle -V classoption=pandoc --listings %option%
+pandoc %name%.md -o %name%.pdf --template=%dir%md2tex\template.tex --pdf-engine=lualatex --pdf-engine-opt=-shell-escape -V documentclass=bxjsarticle -V classoption=pandoc --listings %ARGS%
 
-@REM =番号あり
-@REM pandoc %name%.md -o %name%.pdf --template=C:\Users\yamatora\CustomCommand\md2tex\template.tex --pdf-engine=lualatex --pdf-engine-opt=-shell-escape -V documentclass=bxjsarticle -V classoption=pandoc --listings -N
+@REM ---
+@REM Option
+@REM    -N
+@REM        章番号
+@REM    --toc
+@REM        目次
+@REM    --trace
+@REM        処理内容表示(進捗確認)
+
+@REM ---
+@REM documentclass
+@REM    - bxjsarticle:  章なしレポート
+@REM    - bxjsreport:   章ありレポート
+@REM    - bxjsbook:     書籍
+@REM    - bxjsslide:    スライド
