bc5b337e2d54500be8d8abbe04d8cca29214ceff
yamatora <yt@yamadataiga.com>
2023-06-23 01:34:29 +0900

    fix path

--- video2audio.bat
+++ video2audio.bat
@@ -1,7 +1,8 @@
 @echo off
 
 echo ==Extract audio from video
+set dir=%~dp0
 set input=%~n1
 set ext=%~x1
 
-Python C:\Users\ymtr\CustomCommand\video_audio\extract_audio.py %input% %ext%
+Python %dir%\video_audio\extract_audio.py %input% %ext%
